c5a5b7c8424661af3ba9310c0dc9043bc34eca2e
Phil Chang <lasagna1299@gmail.com>
2023-03-29 00:07:23 +0900

    make ball.gd simpler

--- proj1/ball.gd
+++ proj1/ball.gd
@@ -1,12 +1,5 @@
 extends RigidBody2D
 
-var is_pressed = false
-
-func _integrate_forces(state):
-	if is_pressed:
-		state.apply_impulse(Vector2(0, -1000))
-		is_pressed = false
-	
 func _on_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-		is_pressed = true
+		apply_impulse(Vector2(0, -1000))
